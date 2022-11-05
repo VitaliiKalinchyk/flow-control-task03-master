4eb546cd6c72b7cc86f4c532c2c5ce8a89aaa22c
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-05 18:52:41 +0200

    5 11

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,10 +1,14 @@
 package com.epam.rd.autotasks;
 
+import java.util.stream.Stream;
 
 class LoopStatements {
     public static int sumOfFibonacciNumbers(int n) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        if (n <=0) {
+            throw new IllegalArgumentException();
+        }
+        return Stream.iterate(new int[] {0,1}, array -> new int[] {array[1], array[0] + array[1]})
+                .limit(n)
+                .map(array -> array[0]).mapToInt(Integer::valueOf).sum();
     }
-
 }
