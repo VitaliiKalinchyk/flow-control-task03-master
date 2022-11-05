280cfd40f1238db74cacec7725b0c16c93f5c3e8
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-05 18:54:47 +0200

    5 11

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 class LoopStatements {
     public static int sumOfFibonacciNumbers(int n) {
-        if (n <=0) {
+        if (n <0) {
             throw new IllegalArgumentException();
         }
         return Stream.iterate(new int[] {0,1}, array -> new int[] {array[1], array[0] + array[1]})
